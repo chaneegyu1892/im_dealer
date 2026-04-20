7a85186dbe8cb1a8812ef8f9530eb7749c099e2e
chaneegyu1892 <272663007+chaneegyu1892@users.noreply.github.com>
2026-04-20 03:13:48 -0400

    fix: 관리자 페이지에 'use client' 지시어 추가

--- src/app/(admin)/admin/vehicles/page.tsx
+++ src/app/(admin)/admin/vehicles/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 export const dynamic = 'force-dynamic';
 import { getAdminVehicles, getAdminBrands } from "@/lib/admin-queries";
 import { VehicleManager } from "@/components/admin/vehicles/VehicleManager";
